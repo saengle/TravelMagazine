6b0cea6fdbdc6956bdb747c50fd9b36648870589
saengle <zohv817@gmail.com>
2024-05-26 20:50:12 +0900

    [Add] Restaurant TableView & Cell .swift

---
+++ TravelMagazine/RestaurantTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantTableViewController.swift
+//  TravelMagazine
+//
+//  Created by 쌩 on 5/26/24.
+//
+
+import UIKit
+
+class RestaurantTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = .blue
+    }
+
+}
