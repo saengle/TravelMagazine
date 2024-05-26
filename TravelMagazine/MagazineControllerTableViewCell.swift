1d903d053797b4183ae919ab8f4d72132a266c62
saengle <zohv817@gmail.com>
2024-05-26 16:40:12 +0900

    [Feat] Magazine UI

--- TravelMagazine/MagazineControllerTableViewCell.swift
+++ TravelMagazine/MagazineControllerTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class MagazineControllerTableViewCell: UITableViewCell {
 
+    @IBOutlet var magazineImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
