e7cc0ada6b70cb2d5e43adea737c0b39a4109aa6
saengle <zohv817@gmail.com>
2024-05-27 18:59:24 +0900

    [Add] Travel Cell

---
+++ TravelMagazine/New Group/AdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdTableViewCell.swift
+//  TravelMagazine
+//
+//  Created by 쌩 on 5/27/24.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
