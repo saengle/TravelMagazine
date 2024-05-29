99a481521e75ed20dac774ba76f00dd5774693bc
saengle <zohv817@gmail.com>
2024-05-29 20:15:13 +0900

    [Add] City Model & dummy File

---
+++ TravelMagazine/Model/CityModel.swift
@@ -0,0 +1,16 @@
+//
+//  CityModel.swift
+//  TravelMagazine
+//
+//  Created by 쌩 on 5/29/24.
+//
+
+import Foundation
+
+struct City {
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+}
